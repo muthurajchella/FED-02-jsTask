07bdd73ed6c6a6783986f149635f9ba5087c5c7e
Muthurajchella- <muthuraj.chella@gmail.com>
2022-10-04 22:49:00 +0530

    arry of object

---
+++ product-items/js/main.js
@@ -0,0 +1,11 @@
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", productId);
+
+
+
+function  productId(){
+    let altert = prompt("Give Your Product Id");
+    let count = shop.find(Id)
+}
+
+let shop = [{Id:"001",ProductName:"Mobile", Model:"Redmi6", Price:7000, Ram:"4GB" }, {Id:"002", ProductNmae:"lenova"}]
